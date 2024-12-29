7fb8b83a605af75073d50b7e4c17afc185a4d9b4
punkushal <spycymagar@gmail.com>
2024-12-29 08:45:15 +0545

    mediafile model added for database table

--- media/models.py
+++ media/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
+class MediaFile(models.Model):
+    CATEGORY_CHOICES = [
+        ('Audio','Audio'),
+        ('Video','Video'),
+        ('Image','Image'),
+    ]
+    file = models.FileField(upload_to='uploads/', validators=[FileExtensionValidator(allowed_extensions=['mp3','mp4','jpeg','gif','png'],),],)
+    file_name = models.CharField(max_length=150)
+    file_size = models.IntegerField()
+    file_type = models.CharField(max_length=50)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
