09e9d4cf5e63da4aa056f7e7032fc09cb5016ad2
punkushal <spycymagar@gmail.com>
2024-12-29 09:46:08 +0545

    to view uploaded media detail, path added

--- media/urls.py
+++ media/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.media_list, name='media_list')
+    path("",views.media_list, name='media_list'),
+    path("media/<int:pk>/",views.media_detail, name='media_detail'),
 ]
