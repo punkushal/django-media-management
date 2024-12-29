abce7fa7425fab368a8ce622befa6e3bd48f428e
punkushal <spycymagar@gmail.com>
2024-12-29 16:34:25 +0545

    path for delete & download

--- media/urls.py
+++ media/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path("",views.media_list, name='media_list'),
     path("media/<int:pk>/",views.media_detail, name='media_detail'),
+    path('media/<int:pk>/delete/', views.media_delete, name='media_delete'),
+    path('media/<int:pk>/download/', views.media_download, name='media_download'),
 ]
